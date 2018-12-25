ffe5bfbcdae120bb8a435382408d04f2e628a27d
ksbogdanov <bogdanow90@gmail.com>
2018-12-25 15:49:37 +0300

    init app, ping test

---
+++ src/main/java/ru/skogmark/valhall/MainController.java
@@ -0,0 +1,20 @@
+package ru.skogmark.valhall;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    private static final Logger log = LoggerFactory.getLogger(MainController.class);
+
+    @RequestMapping(method = RequestMethod.GET, path = "/ping")
+    @ResponseBody
+    public String ping() {
+        log.info("MainController ping()");
+        return "ok";
+    }
+}
