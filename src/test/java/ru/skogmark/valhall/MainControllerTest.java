bc989d81ac933d6405a2e8eb322041dd8c29e9e3
ksbogdanov <ksbogdanov@yamoney.ru>
2018-12-26 15:53:31 +0300

    added jtdlib

--- src/test/java/ru/skogmark/valhall/MainControllerTest.java
+++ src/test/java/ru/skogmark/valhall/MainControllerTest.java
@@ -1,5 +1,8 @@
 package ru.skogmark.valhall;
 
+import it.ernytech.tdbot.EasyClient;
+import it.ernytech.tdlib.Response;
+import it.ernytech.tdlib.TdApi;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,5 +25,25 @@ public class MainControllerTest {
         String response = new SerializerAwareHttpRequest(new JacksonObjectMapperSerializerAdapter())
                 .makeRequest(header, null);
         assertEquals("ok", response);
+    }
+
+    @Test
+    public void testConnection() throws Exception {
+        TdApi.TdlibParameters parameters = new TdApi.TdlibParameters();
+        parameters.databaseDirectory = "tdlib";
+        parameters.useMessageDatabase = true;
+        parameters.useSecretChats = true;
+        parameters.apiId = 94575;
+        parameters.apiHash = "a3406de8d171bb422bb6ddf3bbd800e2";
+        parameters.systemLanguageCode = "en";
+        parameters.deviceModel = "Desktop";
+        parameters.systemVersion = "Unknown";
+        parameters.applicationVersion = "1.0";
+        parameters.enableStorageOptimizer = true;
+        EasyClient client = new EasyClient(easyClient -> easyClient.send(new TdApi.SetTdlibParameters(parameters)));
+
+        // test Client.execute
+        Response response = client.execute(new TdApi.GetTextEntities("@telegram /test_command https://telegram.org telegram.me @gif @test"));
+        System.out.println(response);
     }
 }
