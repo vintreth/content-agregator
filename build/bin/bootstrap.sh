ed3a43375e07b4fe0e686abeba6ff970e5fbfbad
svip <bogdanow90@gmail.com>
2019-01-03 23:03:00 +0300

    build configurating

--- build/bin/bootstrap.sh
+++ build/bin/bootstrap.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/bin/java -Dlogging.config=/opt/valhall/conf/logback.xml -Dapp.configLocation=/opt/valhall/conf -jar /opt/valhall/lib/skogmark-valhall-1.0-SNAPSHOT.jar
+/usr/bin/java -Dlogging.config=/opt/valhall/conf/logback.xml -Dapp.configLocation=/opt/valhall/conf -jar /opt/valhall/lib/app.jar
