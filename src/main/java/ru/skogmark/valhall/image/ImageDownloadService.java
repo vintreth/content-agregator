e32aa841c9f89a6bcc8c01c7b78a8c2fa7abe66c
svip <bogdanow90@gmail.com>
2019-01-05 22:46:24 +0300

    commons-io

--- src/main/java/ru/skogmark/valhall/image/ImageDownloadService.java
+++ src/main/java/ru/skogmark/valhall/image/ImageDownloadService.java
@@ -1,11 +1,13 @@
 package ru.skogmark.valhall.image;
 
-import javax.annotation.Nullable;
+import org.apache.commons.io.FileUtils;
+
 import java.net.URI;
 import java.util.Optional;
 
 public class ImageDownloadService {
     public Optional<Image> downloadAndSave(URI imageUri) {
+//        FileUtils.copyURLToFile();
         return Optional.empty(); // todo write code
     }
 }
